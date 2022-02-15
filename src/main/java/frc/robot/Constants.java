41bebaecc2d5717aa7fc9b1bcc8c7ab6d521ceae
Yamilett Estrada-Reyes <36552200+yyestrada-re@users.noreply.github.com>
2022-02-14 18:15:12 -0600

    fixed encoders

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,29 +13,29 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    public static final class gamepadButtons {
+    public static final class GamepadButtons {
         public static int ENCODER_BUTTON = 2;
     }
 
-    public static final class driveModeConstants {
+    public static final class DriveModeConstants {
         public static int JOYSTICK_DRIVE = 0;
         public static int LIMELIGHT_DRIVE = 1;
         public static int ENCODER_DRIVE = 2;
 
     public static final class DriveConstants {
         // left motor ports
-        public static final int upperLeftMotor = 10;
-        public static final int lowerLeftMotor = 7;
+        public static final int UPPER_LEFT_MOTOR = 10;
+        public static final int LOWER_LEFT_MOTOR  = 7;
 
         // right motor ports
-        public static final int upperRightMotor = 9;
-        public static final int lowerRightMotor = 8;
+        public static final int UPPER_RIGHT_MOTOR = 9;
+        public static final int LOWER_RIGHT_MOTOR = 8;
 
         // joystick ports
-        public static final int driveController = 0;
+        public static final int DRIVE_CONTROLLER = 0;
 
         // max output
-        public static final double maxOutput = 0.75;
+        public static final double MAX_OUTPUT = 0.75;
     }
 
     public static final class IntegratedShooterPID {
@@ -61,15 +61,15 @@ public final class Constants {
         public static final double ConversionRate = 2048.0 / 600.0;
     }
 
-    public static final class encoderConstants {
+    public static final class EncoderConstants {
       // wheel constants
       public static int k_UNITS_PREVOLUTION = 2048;
-      public static double WHEEL_DIAMETERFT = 0.5; // 6 inches
+      public static double WHEEL_DIAMETER_FT = 0.5; // 6 inches
 
-      public static double REVOLUTION_PFT = 1 / WHEEL_DIAMETERFT * Math.PI;
+      public static double REVOLUTION_P_FT = 1 / WHEEL_DIAMETER_FT * Math.PI;
 
       // target distance for autonomous
-      public static double TARGET_DISTANCEFT = 8.46; // the distance from the field to the bottom of the vision tape
+      public static double TARGET_DISTANCE_FT = 8.46; // the distance from the field to the bottom of the vision tape
 
       public static double LEFT_SPEED = 0.4;
       public static double RIGHT_SPEED = 0.4;
--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -1,16 +1,15 @@
 package frc.robot.commands;
 
-import frc.robot.subsystems.DriveTrain;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
 
 
 public class Drive extends CommandBase {
 
-  // DriveTrain subsystem to manipulate the DifferentialDrive
   private DriveTrain driveTrainSubsystem;
 
   private NetworkTableEntry ledMode = NetworkTableInstance.getDefault().getTable("limelight-console").getEntry("ledMode");
@@ -29,20 +28,19 @@ public class Drive extends CommandBase {
   public void execute() {
     SmartDashboard.putNumber("DriveTrain driveMode is toggled to:", DriveTrain.driveMode);
 
-    if(DriveTrain.driveMode == Constants.driveModeConstants.LIMELIGHT_DRIVE) { // 1
+    if(DriveTrain.driveMode == Constants.DriveModeConstants.LIMELIGHT_DRIVE) { // 1
       ledMode.setNumber(1);
     }
-    else if(DriveTrain.driveMode == Constants.driveModeConstants.ENCODER_DRIVE) { // 2
+    else if(DriveTrain.driveMode == Constants.DriveModeConstants.ENCODER_DRIVE) { // 2
       if(driveTrainSubsystem.encoderDrive() == true) { // while encoders have not yet reached target distance and need to continue measuring...
         driveTrainSubsystem.encoderDrive();
       }
       else {
-        DriveTrain.driveMode = Constants.driveModeConstants.JOYSTICK_DRIVE; // once target has been reached, toggle to manual
+        DriveTrain.driveMode = Constants.DriveModeConstants.JOYSTICK_DRIVE; // once target has been reached, toggle to manual
         driveTrainSubsystem.resetEncoders();
       }
     }
     else { // 0
-      SmartDashboard.putNumber("DriveTrain driveMode has been changed to: ", DriveTrain.driveMode);
       driveTrainSubsystem.driveWithJoystick();
       driveTrainSubsystem.resetEncoders();
       ledMode.setNumber(0); // turn off limelight
