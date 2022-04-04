a93f71b54ab69478996d0c01b64bd42d6eededfc
Yamilett Estrada-Reyes <93739159+Aidan747@users.noreply.github.com>
2022-04-04 18:18:06 -0500

    Monday changes

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -1,8 +1,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,23 +9,24 @@ import frc.robot.Constants;
 
 public class Indexer extends SubsystemBase {
   private WPI_TalonFX verticalIndexerMotor;
-  private WPI_TalonFX horizontalIndexerMotor;
+  private WPI_TalonSRX horizontalIndexerMotor;
 
-  private CANSparkMax intakeMotor = new CANSparkMax(Constants.IndexerConstants.INTAKE_MOTOR, MotorType.kBrushed);
+  private WPI_TalonSRX intakeMotor = new WPI_TalonSRX(Constants.IndexerConstants.INTAKE_MOTOR);
 
   private double indexerSpeed = 0.3; 
-  private double intakeSpeed = 0.6;
+  private double intakeSpeed = 0.5;
 
-  public Indexer(WPI_TalonFX verticalIndexerMotor, WPI_TalonFX horizontalIndexerMotor) {
+  public Indexer(WPI_TalonFX verticalIndexerMotor, WPI_TalonSRX horizontalIndexerMotor) {
     //Indexer splits path from intake to shooter into vertical and horizontal belt systems. 
     this.verticalIndexerMotor = verticalIndexerMotor;
     this.horizontalIndexerMotor = horizontalIndexerMotor;
+    stop();
   }
 
   /*enables intake and bottom indexer*/
   public void absorb() {
     SmartDashboard.putString("absorb?", "jas");
-    intakeMotor.set(-intakeSpeed);
+    intakeMotor.set(intakeSpeed);
     horizontalIndexerMotor.set(indexerSpeed);
     verticalIndexerMotor.set(0); // don't want it to be running
   }
@@ -34,13 +34,18 @@ public class Indexer extends SubsystemBase {
   /* reverses bottom indexer in case a ball gets stuck */
   public void unabsorb() {
     SmartDashboard.putString("absorb?", " not jas");
-    intakeMotor.set(intakeSpeed);
-    verticalIndexerMotor.set(-indexerSpeed);
+    intakeMotor.set(-intakeSpeed);
+    verticalIndexerMotor.set(indexerSpeed);
     horizontalIndexerMotor.set(-indexerSpeed);
   }
 
   public void enableVerticalIndexer() {
-    verticalIndexerMotor.set(indexerSpeed);
+    verticalIndexerMotor.set(-indexerSpeed);
+  }
+
+  public void stop() {
+    verticalIndexerMotor.set(0);
+    horizontalIndexerMotor.set(0);
   }
 
   @Override
